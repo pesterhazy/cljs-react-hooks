8ef5c9fcfb5e28ec15e4d3d06c67fe2350f41228
Paulus Esterhazy <pesterhazy@gmail.com>
2019-02-07 18:24:05 +0100

    WIP

--- src/react/sandbox.cljs
+++ src/react/sandbox.cljs
@@ -1,40 +1,19 @@
 (ns ^:figwheel-hooks react.sandbox
-  (:require
-   [goog.dom :as gdom]
-   [react :as react]
-   [react-dom :as react-dom]
-   [sablono.core :as sab :include-macros true]))
+  (:require [react :as react] ;; presumably for side-effects
+            [react-dom :as react-dom]))
 
-(println "This text is printed from src/react/sandbox.cljs. Go ahead and edit it and see reloading in action.")
+(defn e
+  [el props & children]
+  (apply js/React.createElement el (clj->js props) children))
 
-(defn multiply [a b] (* a b))
+(defn mount
+  []
+  (js/ReactDOM.render
+   (e "div" nil "Hello, world!!")
+   (js/document.getElementById "app")))
 
+;; This is called once
+(defonce init (mount))
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
-
-(defn get-app-element []
-  (gdom/getElement "app"))
-(defn hello-world [state]
-  (sab/html [:div
-             [:h1 (:text @state)]
-             [:h3 "Welcome"]]))
-
-(defn mount [el]
-  (js/ReactDOM.render (hello-world app-state) el))
-
-(defn mount-app-element []
-  (when-let [el (get-app-element)]
-    (mount el)))
-
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
-(mount-app-element)
-
-;; specify reload hook with ^;after-load metadata
-(defn ^:after-load on-reload []
-  (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn ^:after-load reload []
+  (mount))
--- test/react/sandbox_test.cljs
+++ test/react/sandbox_test.cljs
@@ -1,10 +1,2 @@
 (ns react.sandbox-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [react.sandbox :refer [multiply]]))
-
-(deftest multiply-test
-  (is (= (* 1 2) (multiply 1 2))))
-
-(deftest multiply-test-2
-  (is (= (* 75 10) (multiply 10 75))))
+  (:require [cljs.test :refer-macros [deftest is testing]]))
