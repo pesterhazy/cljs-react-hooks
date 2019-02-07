15ba74a7823bd5a02d9d25d44fab40ab2c432e24
Paulus Esterhazy <pesterhazy@gmail.com>
2019-02-07 18:27:03 +0100

    WIP

--- src/react/sandbox.cljs
+++ src/react/sandbox.cljs
@@ -1,15 +1,28 @@
-(ns ^:figwheel-hooks react.sandbox
-  (:require [react :as react] ;; presumably for side-effects
-            [react-dom :as react-dom]))
+(ns ^:figwheel-hooks react.sandbox)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn e
   [el props & children]
   (apply js/React.createElement el (clj->js props) children))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn Example
+  []
+  (let [[count setCount] (js/React.useState 0)]
+    (e "div" nil
+       (e "p" nil "You clicked " count " times")
+       (e "button"
+          {:onClick
+           (fn [e]
+             (setCount (inc count)))}
+          "Click Me"))))
+
 (defn mount
   []
   (js/ReactDOM.render
-   (e "div" nil "Hello, world!!")
+   ((js/React.createFactory Example))
    (js/document.getElementById "app")))
 
 ;; This is called once
